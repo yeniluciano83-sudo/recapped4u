68a2c649f9ef18d72ebb7d7fa00a43284bc907b1
yeniluciano83-sudo <yeniluciano83@gmail.com>
2026-08-13 18:03:10 -0400

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const isAuthed = req.cookies.get("dashboard_auth")?.value === process.env.DASHBOARD_PASSWORD;
+  if (!isAuthed) {
+    return NextResponse.redirect(new URL("/dashboard-login", req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/dashboard/:path*",
+};
